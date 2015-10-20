63945ff50858092e4c7b9d2d3f98d11cf3571499
Mate Antunovic <mate.a@blackpearlmail.com>
2015-10-20 20:26:29 +1300

    Implement parsing correctly

--- src/message_parser.rs
+++ src/message_parser.rs
@@ -11,14 +11,10 @@ fn parse_message(message_str : String) -> Result<Message, (String, usize)>  {
 
 	if (chars.len() > 0 && chars[0] == ':') {
 		prefix = Some(try!(parse_prefix(&chars, &mut index)));
-	}else{
-		println!("THIS IS CHAR {}", chars[0]);
 	}
 
-	println!("Parsing command...");
 	let command = try!(parse_command(&chars, &mut index));
 
-	println!("Parsing params...");
 	let parameters = try!(parse_parameters(&chars, &mut index));
 
 	eat_char(&chars, '\r', &mut index);
@@ -62,7 +58,7 @@ fn parse_host(message_str : &Vec<char>, index : &mut usize) -> Result<String, (S
 fn parse_command(message_str : &Vec<char>, index : &mut usize) -> Result<Command, (String, usize)> {
 	let mut command_str = String::new();
 	if message_str[*index].is_digit(10) {
-		while *index < message_str.len() && (message_str[*index]).is_digit(10) {
+		while *index < message_str.len() - 2 && (message_str[*index]).is_digit(10) {
 			command_str.push(message_str[*index]);
 			*index = *index + 1;
 		}
@@ -70,7 +66,7 @@ fn parse_command(message_str : &Vec<char>, index : &mut usize) -> Result<Command
 			command : command_str
 		});
 	}else if message_str[*index].is_alphabetic() {
-		while *index < message_str.len() && message_str[*index].is_alphabetic(){
+		while *index < message_str.len() - 2 && message_str[*index].is_alphabetic(){
 			command_str.push(message_str[*index]);
 			*index = *index + 1;
 		}
@@ -99,21 +95,41 @@ fn parse_parameters(message_str : &Vec<char>, index : &mut usize) -> Result<Vec<
 
 fn parse_parameters_rec<'a>(message_str : &Vec<char>, 
 	index : &mut usize, parsedParams : &'a mut Vec<String>) -> Result<&'a Vec<String>, (String, usize)> {
-	if *index >= message_str.len() {
+	if *index >= message_str.len() - 2 {
 			return Ok(parsedParams);
 	}
 	eat_char(message_str, ' ', index);
+	if (message_str[*index] == ':') {
+		println!("parsing trailing");
+		eat_char(message_str, ':', index);
+		return parse_trailing_parameters_rec(message_str, index, parsedParams);
+	}
 	let middle = try!(parse_middle(message_str, index));
+	println!("Parsed param {}", middle);
 	parsedParams.push(middle);
-	if *index < message_str.len() - 1 {
+	if *index < message_str.len() - 2 {
+		println!("Parsing more... {} {}", *index, message_str.len());
 		return parse_parameters_rec(message_str, index, parsedParams);
 	}
+	println!("Done parsing");
 	return Ok(parsedParams);
+}
+
+fn parse_trailing_parameters_rec<'a>(message_str : &Vec<char>, 
+	index : &mut usize, parsedParams : &'a mut Vec<String>) -> Result<&'a Vec<String>, (String, usize)> {
+	let mut param = String::new();
+	while *index < message_str.len() - 2 && (is_valid_middle_char(message_str[*index]) || message_str[*index] == ' ' || message_str[*index] == ':') {
+		param.push(message_str[*index]);
+		*index += 1;
+	}
+	println!("Parsed end param {}", param);
+	parsedParams.push(param);
+	Ok(parsedParams)
 }
 
 fn parse_middle(message_str : &Vec<char>, index : &mut usize) -> Result<String, (String, usize)> {
 	let mut middle = String::new();
-	while is_valid_middle_char(message_str[*index]) {
+	while *index < message_str.len() - 2 && is_valid_middle_char(message_str[*index]) {
 		middle.push(message_str[*index]);
 		*index += 1;
 	}
@@ -149,7 +165,8 @@ mod message_parser_tests {
 		let command = String::from("NICK");
 		let message = parse_message(messageStr).unwrap();
 		assert_eq!(Command::LetterCommand {command : command}, message.command);
-		assert_eq!(Prefix::ServerNamePrefix {name : host}, message.prefix.unwrap());
+		assert_eq!(Prefix::ServerNamePrefix {name : host.clone()}, message.prefix.unwrap());
+		assert_eq!("nico676", &message.parameters[0]);
 	}
 
 	#[test]
@@ -186,7 +203,16 @@ mod message_parser_tests {
 		assert_eq!(message.parameters, vec!["jono3".to_string(), "0".to_string(), "*".to_string(), "John Third".to_string()]);
 	}
 
+	#[test]
+	fn test_usermessage_parses_3() {
+		let message = String::from("USER gary_l 0 * :Gary Larson\r\n");
+		let message = parse_message(message).unwrap();
+		assert!(message.prefix.is_none());
+		assert_eq!(Command::LetterCommand{command : "USER".to_string()}, message.command);
+	}
+
 }
 
 //:irc.freenode.net NICK nico676
-USER jono3 0 * :John Third
+//USER jono3 0 * :John Third
+//
