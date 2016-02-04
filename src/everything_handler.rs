e5304b4db2ccef56693b415eb58c64f99e5cd677
Mate Antunovic <mate.a@blackpearlmail.com>
2016-02-05 11:46:55 +1300

    Add a way to process message bytes

--- src/everything_handler.rs
+++ src/everything_handler.rs
@@ -5,14 +5,19 @@ use std::sync::mpsc;
 use std::thread;
 use std::io;
 use std::io::{BufRead, BufReader, BufWriter, Write, Read};
+use std::iter;
 use std::net::{ToSocketAddrs};
 use std::env;
 use std::path::PathBuf; 
 use std::net::{SocketAddr};
-use std::str::FromStr;
+use std::str;
+use std::str::{FromStr};
 
 use message_util::*;
 use session_state::{Session, SessionState};
+
+use std::mem;
+use std::iter::IntoIterator;
 
 const NETWORK: Token = Token(0);
 const KEYBOARD: Token = Token(1);
@@ -22,18 +27,26 @@ pub struct EverythingHandler {
     pub stream : TcpStream, 
     lines_in : Vec<String>, 
     lines_out : Vec<String>,
-    session : Session
+    session : Session,
+    buf : Vec<u8>,
+    line_in_progress : Vec<u8>
 }
 
 impl EverythingHandler {
     pub fn new(recv : mpsc::Receiver<String>, stream : TcpStream, session : Session) 
         -> EverythingHandler {
+        let mut buf = Vec::with_capacity(256);
+            for _ in 0..256 {
+                buf.push(0);
+        };
         EverythingHandler {
             recv : recv,
             stream : stream,
             lines_in : Vec::new(),
             lines_out : Vec::new(),
-            session : session
+            session : session,
+            buf : buf,
+            line_in_progress : Vec::new()
         }
     }
 
@@ -41,27 +54,20 @@ impl EverythingHandler {
         let action = match token {
             NETWORK => {
                 if event_set.is_readable() {
-                    let mut buf = Vec::with_capacity(256);
-                    for _ in 0..256 {
-                        buf.push(0);
-                    }
-
-                    match self.stream.try_read(&mut buf) {
+                    let mut buf = &mut self.buf;
+                    match self.stream.try_read(buf) {
                         Ok(option) => {
                             if let Some(bytes_read) = option {
-                                println!("Got: {:?}", bytes_read);
+                                self.line_in_progress.extend(buf[0..bytes_read].iter());
                             }
-                            else {
-                                println!("Hmmm... nothing in the buffer: {:?}", buf)
-                            }
+
+                            let message_bits = process_str(&self.line_in_progress);
                         },
-                        Err(err) => println!("Some error: {:?}", err)   
+                        Err(err) => {}   
                     }
                 }
                 else if event_set.is_writable() {
-                    println!("Gonna try to log write");
                     if let SessionState::Connected_LoginReady = self.session.state {
-                        println!("Gonna try to log in");
                         let nick_result = self.stream.write(&nick_message(&self.session.nick).to_message_bytes_rn());
                         let name_result = self.stream.write(&user_message(&self.session.name, 
                             &self.session.name)
@@ -70,27 +76,25 @@ impl EverythingHandler {
                         if nick_result.is_ok() && name_result.is_ok() {
                             self.session.state = SessionState::Connected_LoginWaiting;
                         }
-                        println!("{:?} {:?}", nick_result, name_result);
                     }
                 }
                 else if event_set.is_hup() {
-                    println!("Holy shit they hung up")
+                    //TODO Need to recconnect or exit here
                 }
                 else if event_set.is_error() {
-                    println!("Holy shit an error occurred")
+                    //TODO need to handle the error here.
                 }
-                None
             },
             KEYBOARD => {
                 let line = self.recv.try_recv();
                 if line.is_ok() {
-                    Some(parse_input_line(&line.unwrap()))
+                    let action = Some(parse_input_line(&line.unwrap()));
                 }
                 else {
-                    None
+                    
                 }
             },
-            _ => None
+            _ => {}
         };
     }
 }
@@ -106,4 +110,29 @@ impl Handler for EverythingHandler {
     fn notify(&mut self, _loop: &mut EventLoop<EverythingHandler>, token: Token) {
         self.handle_action(token, EventSet::readable());
     }
+}
+
+fn process_str(data_in : &Vec<u8>) -> (Vec<String>, Vec<u8>) {
+    let mut state : bool = false; 
+    let mut out_str = Vec::new();
+    let mut buf = Vec::new();
+    for b in data_in {
+        match (b, state) {
+            (&10, false) => {
+                state = true;
+                let mut buf_for_string = Vec::new();
+                mem::swap(&mut buf_for_string, &mut buf);
+                out_str.push(String::from_utf8(buf_for_string).unwrap());
+            }
+            (&13, true) => {
+
+                state = false;
+            }
+            (&b, _) => {
+                state = false;
+                buf.push(b);
+            }  
+        }
+    }
+    (out_str, buf)
 }
